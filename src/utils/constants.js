09083681e15bf3b00a8bac2ef6a443f2a3d839b4
Mai Thanh Bình <78849370+cfgamehay@users.noreply.github.com>
2025-03-10 01:52:46 +0700

    asd

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,8 +3,11 @@ export const WHITELIST_DOMAINS = [
   'http://localhost:3000',
   'http://localhost:5501',
   'http://127.0.0.1:5501',
-  'https://ecommernceapi.onrender.com'
+  'https://ecommernceapi.onrender.com',
+  'https://ecommernceapi.onrender.com/v1'
 ]
+
+ex
 
 export const BOADRS_TYPE ={
   PUBLIC: 'public',
